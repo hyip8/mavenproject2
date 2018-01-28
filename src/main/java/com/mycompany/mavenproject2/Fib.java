7554f61adb3cf3dd57964315e314b7ede441c7e3
Henry Yip <hc.yip88@gmail.com>
2018-01-27 23:45:04 -0800

    first commit

---
+++ src/main/java/com/mycompany/mavenproject2/Fib.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author Henry Yip
+ */
+public class Fib {
+    
+    public int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        
+        int prev0 = 0;
+        int prev1 = 1;
+        int next;
+        for (int i=0;i <n-1; i++) {
+            next = prev0 + prev1;
+            prev0 = prev1;
+            prev1 = next;
+        }
+        return prev0 + prev1;
+    }
+    
+    public void fib_prn(int n) {
+        int r = fib(n);
+        System.out.println("fib("+n+")=" + r);
+    }
+    
+    public static void main(String[] argc) {
+        Fib fc = new Fib();
+        fc.fib_prn(0);
+        fc.fib_prn(1);
+        fc.fib_prn(4);
+        
+    }
+}
---
+++ src/test/java/NewJUnitTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.mycompany.mavenproject2.Fib;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Henry Yip
+ */
+public class NewJUnitTest {
+    
+    private Fib fib = null;
+    
+    public NewJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        fib = new Fib();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void test1() {
+        assert(fib.fib(0) == 0);
+        assert(fib.fib(5) == 8);   
+    }
+}
